04e9689e0ce3ccaca448104a4c3c18249b67a8cb
pritam-bots <mrpritam2136@gmail.com>
2025-09-13 01:55:32 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -44,7 +44,7 @@ def circle(pfp, size=(500, 500)):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("AnonXMusic/assets/welcomex12.png")
+    background = Image.open("AnonXMusic/assets/welcomex14.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((825, 824))
