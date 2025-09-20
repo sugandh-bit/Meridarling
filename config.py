4fc028538fe039750fdd81ee7fa498b24de3ce93
sugandh-bit <mrsugandh32@gmail.com>
2025-09-20 13:20:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://ibb.co/zVgT2N6P"
 )
 PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/edd388a42dd2c499fd868.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/jk6l11.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
 STREAM_IMG_URL = "https://graph.org/file/ff2af8d4d10afa1baf49e.jpg"
